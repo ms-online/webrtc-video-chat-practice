94c42fb3be5390eb739ea69c4f4aba1cac48c0b2
ms-online <27732357@qq.com>
2022-11-04 15:36:17 +0800

    lesson-07

--- video-chat-frontend/src/pages/Dashboard/Dashboard.js
+++ video-chat-frontend/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import logo from '../../resources/logo.png';
+import './Dashboard.css';
 
 const Dashboard = () => {
-  return <div>欢迎来到视频聊天应用</div>;
+  return (
+    // 外层容器
+    <div className='dashboard_container background_main_color'>
+      {/* 左侧部分  */}
+      <div className='dashboard_left_section'>
+        {/* 内容介绍 */}
+        <div className='dashboard_content_container'>内容</div>
+        <div className='dashboard_rooms_container background_secondary_color'>
+          房间
+        </div>
+      </div>
+      {/* 右侧部分 */}
+      <div className='dashboard_right_section background_secondary_color'>
+        {/* 活跃用户 */}
+        <div className='dashboard_active_users_list'>活跃用户</div>
+        {/* logo */}
+        <div className='dashboard_logo_container'>
+          <img className='dashboard_logo_image' src={logo} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
